6176bff74eb6f59bfaad076162526597950fb82f
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-03-15 20:13:27 +0000

    Update main.css and FooterModule.vue

--- src/components/FooterModule.vue
+++ src/components/FooterModule.vue
@@ -9,29 +9,16 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="footer-module">
-      <p>&copy; Copyright TeamLitho 2021 - {{ currentYear }}
-        
-         <ul>
-        <li>
-          <router-link
-            style="color: lightgray; text-decoration: none; font-weight: bold"
-            to="/LegalInformation/LegalInformation"
-            >Legal Information
-            <span><i class="bi bi-arrow-up-right-square"></i> </span>
-          </router-link>
-        </li>
-        <li>
-          <router-link
-            style="color: lightgray; text-decoration: none; font-weight: bold"
-            to="/IssuesReporting/FoundAnIssues/"
-            >Found An Issues
-            <span><i class="bi bi-arrow-up-right-square"></i> </span>
-          </router-link>
-        </li>
-      </ul>
-      </p>
-    </div>
+  <div class="footer">
+    <p class="text-info">
+      &copy; TeamLitho PartnerSite 2021 - {{ currentYear() }} All Right
+      Reserved.
+      <router-link class="textlink" to="/LegalInformation/LegalInformation"
+        >Legal Information</router-link
+      >
+      <router-link class="textlink" to="/IssuesReporting/FoundAnIssues/"
+        >Found An Issues!</router-link
+      >
+    </p>
   </div>
 </template>
